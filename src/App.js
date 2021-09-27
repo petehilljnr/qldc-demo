cc7d10df407369349be7fec387a729dc13986d50
Peter Hill <petehilljnr@gmail.com>
2021-09-27 22:36:47 +1300

    updated csv

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Overview from './pages/Overview';
 import { Reports, ReportsOne, ReportsTwo, ReportsThree } from './pages/Reports';
 import Team from './pages/Team';
+import Dashboard from './pages/Dashboard';
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
       <Switch>
         <Route path='/overview' exact component={Overview} />
         <Route path='/reports' exact component={Reports} />
-        <Route path='/reports/reports1' exact component={ReportsOne} />
+        <Route path='/reports/dashboard' exact component={Dashboard} />
         <Route path='/reports/reports2' exact component={ReportsTwo} />
         <Route path='/reports/reports3' exact component={ReportsThree} />
         <Route path='/team' exact component={Team} />
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import crossfilter from 'crossfilter2'
 import * as d3 from 'd3'
-import csv from './ndx.csv'
+
 import {
   BarChart,
   PieChart,
@@ -16,16 +16,25 @@ function Dashboard() {
     const [cx, setCx] = useState(null);
 
     useEffect(()=> {
-        fetch(shapes_url)
-        .then(function(response) {
-            return response.json();
-        })
-        .then(function(myJson) {
-            let x = myJson.map(d => ({ ...d, count: 0 }));
-            setShapes(x)
-        });
-}
+        ;(async () => {
+            const data = await d3.csv(csv)
+            const dateFormatSpecifier = '%m/%d/%Y'
+            const dateFormatParser = d3.timeParse(dateFormatSpecifier)
+            data.forEach(d => {
+              d.dd = dateFormatParser(d.date)
+              d.month = d3.timeMonth(d.dd) // pre-calculate month for better performance
+              d.close = +d.close // coerce to number
+              d.open = +d.open
+            })
+            const cx = crossfilter(data)
+            setCx(cx)
+        })()
     },[])
+
+    if (!cx) {
+        return <p> Loading Data </p>
+    }
+
 }
 
 export default Dashboard;
